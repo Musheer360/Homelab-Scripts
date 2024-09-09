f75508e491ef4fe3930b7d1d597cc17130dd2958
Musheer Alam <musheeralam360@gmail.com>
2024-09-09 22:44:00 +0530

    Update .keepalive_ssh.sh

--- .keepalive_ssh.sh
+++ .keepalive_ssh.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+
+# Check if sshd is running, and start it if not
 if ! pgrep -x "sshd" > /dev/null
 then
     sshd
+fi
+
+# Check if the redirection rule already exists
+if su -c "/system/bin/iptables -t nat -L PREROUTING -n -v | grep -q 'tcp dpt:22 redir ports 8022'"; then
+    # Rule already exists, no action needed
+    :
+else
+    # Add the redirection rule
+    su -c "/system/bin/iptables -t nat -A PREROUTING -p tcp --dport 22 -j REDIRECT --to-port 8022"
 fi
